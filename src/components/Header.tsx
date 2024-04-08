41dc0614ad22dbef0bac5cd30d36663f2f552f1d
James Rivera <112741353+JamesRiveran@users.noreply.github.com>
2024-04-07 22:33:02 -0600

    Components y config de app

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+'use client'
+function Header() {
+    return (
+        <nav className="shadow-sm bg-white flex flex-col py-2">
+            <h2 className="text-gray-500 text-3xl">Weather App</h2>
+        </nav>
+    )
+}
+
+export default Header
